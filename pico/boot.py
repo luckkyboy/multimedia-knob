ee2281cca0edad5905cec239210ad6c5ee03f116
luckkyboy <farboy0509@gmail.com>
2025-01-24 10:44:04 +0800

    enable storage in boot.py

--- pico/boot.py
+++ pico/boot.py
@@ -1,3 +1,27 @@
-import storage
+import adafruit_rgbled, board, digitalio, storage, time
 
-storage.disable_usb_drive()
+SW_PIN = board.GP4
+RED_LED = board.GP20
+GREEN_LED = board.GP19
+BLUE_LED = board.GP18
+
+sw = digitalio.DigitalInOut(SW_PIN)
+sw.direction = digitalio.Direction.INPUT
+sw.pull = digitalio.Pull.UP
+
+led = adafruit_rgbled.RGBLED(RED_LED, GREEN_LED, BLUE_LED)
+#
+# IMPORTANT FOR EDITING THIS SCRIPT
+# In CircuitPython 9.0.0 and later, CIRCUITPY also becomes read-write to your program if it is not visible over USB.
+# We will disable usb drive in boot.py
+# Press down the knob while plugging in to be able to upload the new code to CIRCUITPY.
+#
+# if CIRCUITPY storage enabled, blink led to RED
+#
+if sw.value != 0:
+    storage.disable_usb_drive()
+else:
+    led.color = 0xFF0000
+    time.sleep(1)
+    led.color = 0xFF0000
+    time.sleep(1)
